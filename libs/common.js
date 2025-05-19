ef1e76d88d3ec532bc688efff3727c6467a33d3e
Puhach-Yu <puhach-yu@foxteam.digital>
2025-05-19 03:51:21 +0300

    Первый коммит

---
+++ libs/common.js
@@ -0,0 +1,29 @@
+let swiperInstance = null;
+
+function swiperCard() {
+	if (window.innerWidth <= 768) {
+		if (!swiperInstance) {
+			swiperInstance = new Swiper('.swiper', {
+				direction: 'horizontal',
+				autoHeight: false,
+				freeMode: false,
+				slidesPerView: 1.2,
+				spaceBetween: 32,
+				loop: false,
+				breakpoints: {
+					480: {
+						slidesPerView: 1.7,
+					}
+				}
+			});
+		}
+	} else {
+		if (swiperInstance) {
+			swiperInstance.destroy(true, true);
+			swiperInstance = null;
+		}
+	}
+}
+
+window.addEventListener("resize", swiperCard);
+document.addEventListener("DOMContentLoaded", swiperCard);
