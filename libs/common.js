982da8397b4de706647bf34a096b54ecb5522d0c
Puhach-Yu <puhach-yu@foxteam.digital>
2025-05-20 04:44:17 +0300

    st

--- libs/common.js
+++ libs/common.js
@@ -1,27 +1,22 @@
-let swiperInstance = null;
-
+let swiperInstance = null; 
 function swiperCard() {
-	if (window.innerWidth <= 768) {
-		if (!swiperInstance) {
-			swiperInstance = new Swiper('.swiper', {
-				direction: 'horizontal',
-				autoHeight: false,
-				freeMode: false,
-				slidesPerView: 1.2,
-				spaceBetween: 32,
-				loop: false,
-				breakpoints: {
-					480: {
-						slidesPerView: 1.7,
-					}
+	if (window.innerWidth <= 767) {
+		swiperInstance = swiperInstance || new Swiper('.swiper', {
+			direction: 'horizontal',
+			autoHeight: false,
+			freeMode: false,
+			slidesPerView: 1.2,
+			spaceBetween: 32,
+			loop: false,
+			breakpoints: {
+				480: {
+					slidesPerView: 1.7,
 				}
-			});
-		}
-	} else {
-		if (swiperInstance) {
-			swiperInstance.destroy(true, true);
-			swiperInstance = null;
-		}
+			}
+		});
+	} else if (swiperInstance) {
+		swiperInstance.destroy(true, true);
+		swiperInstance = null;
 	}
 }
 
